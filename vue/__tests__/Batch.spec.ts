2d97e17daf2fa7b8485053397ea6a1f8f640a415
felipemuner <felipe.muner@gmail.com>
2020-06-27 11:23:01 +0700

    set up Batch.spec.ts and todo tests

---
+++ vue/__tests__/Batch.spec.ts
@@ -0,0 +1,15 @@
+import { useState, self } from "../src";
+import { computed } from "vue";
+import { h } from "vue";
+
+it.todo('object: should rerender used via nested batch update')
+
+it.todo('object: should rerender used via nested batch merge')
+
+it.todo('object: should rerender used via nested batch double')
+
+it.todo('object: should rerender used via nested batch promised')
+
+it.todo('object: should rerender used via nested batch promised manual')
+
+it.todo('object: should rerender used via scoped batched updates')
