d0d09f49e5d492881681c9c594955d6fc8d0f6d3
felipemuner <felipe.muner@gmail.com>
2020-06-27 12:33:47 +0700

    set up Object.spec.ts and todo tests

--- vue/__tests__/Object.spec.ts
+++ vue/__tests__/Object.spec.ts
@@ -2,10 +2,18 @@ import { useState, self } from "../src";
 import { computed } from "vue";
 import { h } from "vue";
 
-it.todo('')
-
-it.todo('')
-
-it.todo('')
-
-it.todo('')
+it.todo('object: should rerender used')
+it.todo('object: should rerender used null')
+it.todo('object: should rerender used property-hiphen')
+it.todo('object: should rerender used (boolean-direct)')
+it.todo('object: should rerender used via nested')
+it.todo('object: should not rerender used symbol properties')
+it.todo('object: should rerender used when set to the same')
+it.todo('object: should rerender when keys used')
+it.todo('object: should rerender unused when new element')
+it.todo('object: should not rerender unused property')
+it.todo('object: should not rerender unused self')
+it.todo('object: should delete property when set to none')
+it.todo('object: should auto save latest state for unmounted')
+it.todo('object: should set to null')
+it.todo('object: should denull')
