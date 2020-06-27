a1ef802560b33e231681a523fc5266ed73a803db
felipemuner <felipe.muner@gmail.com>
2020-06-27 12:11:26 +0700

    set up Complex.spec.ts and todo tests

---
+++ vue/__tests__/Complex.spec.ts
@@ -0,0 +1,12 @@
+import { useState, self } from "../src";
+import { computed } from "vue";
+import { h } from "vue";
+
+it.todo('complex: should rerender used')
+it.todo('complex: should rerender used via nested')
+it.todo('complex: should rerender used when set to the same')
+it.todo('complex: should rerender unused when new element')
+it.todo('complex: should not rerender unused property')
+it.todo('complex: should not rerender unused self')
+it.todo('complex: should delete property when set to none')
+it.todo('complex: should auto save latest state for unmounted')
