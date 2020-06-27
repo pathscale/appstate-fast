b11cc04871609be67fe843225fd384acfc5460e5
felipemuner <felipe.muner@gmail.com>
2020-06-27 12:44:11 +0700

    set up Primitive.spec.ts and todo tests

--- vue/__tests__/Primitive.spec.ts
+++ vue/__tests__/Primitive.spec.ts
@@ -2,10 +2,16 @@ import { useState, self } from "../src";
 import { computed } from "vue";
 import { h } from "vue";
 
-it.todo('')
-
-it.todo('')
-
-it.todo('')
-
-it.todo('')
+it.todo('primitive: should rerender used')
+it.todo('primitive: should rerender used (boolean)')
+it.todo('primitive: should rerender used (null)')
+it.todo('primitive: should rerender used (undefined)')
+it.todo('primitive: should rerender used (global null)')
+it.todo('primitive: should rerender used (global undefined)')
+it.todo('primitive: should rerender used when set to the same')
+it.todo('primitive: should rerender when keys used')
+it.todo('primitive: should not rerender unused')
+it.todo('primitive: global state')
+it.todo('primitive: global state created locally')
+it.todo('primitive: stale state should auto refresh')
+it.todo('primitive: state value should be the latest')
