0d99acd5af408f7f96758321c52016849e7be45a
felipemuner <felipe.muner@gmail.com>
2020-06-27 12:20:21 +0700

    set up Merge.spec.ts and todo tests

---
+++ vue/__tests__/Merge.spec.ts
@@ -0,0 +1,18 @@
+import { useState, self } from "../src";
+import { computed } from "vue";
+import { h } from "vue";
+
+it.todo('primitive: should rerender used after merge update')
+it.todo('string: should rerender used after merge update')
+it.todo('object: should rerender used after merge update')
+it.todo('object: should rerender used after merge insert')
+it.todo('object: should rerender used after merge delete')
+it.todo('object: should rerender used after merge complex')
+it.todo('object: should not rerender unused after merge update')
+it.todo('array: should rerender used after merge update')
+it.todo('array: should rerender used after merge insert')
+it.todo('array: should rerender used after merge concat')
+it.todo('array: should rerender used after merge concat (scoped)')
+it.todo('array: should rerender used after merge delete')
+it.todo('array: should rerender used after merge complex')
+it.todo('array: should not rerender unused after merge update')
---
+++ vue/__tests__/Object.spec.ts
@@ -0,0 +1,11 @@
+import { useState, self } from "../src";
+import { computed } from "vue";
+import { h } from "vue";
+
+it.todo('')
+
+it.todo('')
+
+it.todo('')
+
+it.todo('')
