33a88c528f2258a5af54fd5fc10901d11bb0b42c
felipemuner <felipe.muner@gmail.com>
2020-06-27 17:58:04 +0700

    error.spec.ts trying to do in many different ways, need help

--- vue/__tests__/Error.spec.ts
+++ vue/__tests__/Error.spec.ts
@@ -1,21 +1,37 @@
-import { useState, self } from "../src";
-import { computed } from "vue";
+import { useState, self, State } from "../src";
+import { mount } from "@vue/test-utils";
 import { h } from "vue";
 
-test('error: should not allow set to another state value', async () => {
-    const state1 = h(() => {
-        return useState({ prop1: [0, 0] })
+// test('error: should not allow set to another state value', async () => {})
+
+// it.todo('error: should not allow create state from another state value')
+
+// it.todo('error: should not allow create state from another state value (nested)')
+
+test('error: should not allow serialization of statelink', async () => {
+    let state1: State<{ prop1: number[] }> = {} as any;
+
+    const wrapper = mount({
+        setup(){
+            state1 = useState({ prop1: [0, 0] });
+            return { state1 }
+        }        
     });
 
-    const state2 = h(() => {
-        return useState({ prop2: [0, 0] })
-    });
+    // const wrapper = mount({
+    //     setup() {
+    //         state1 = useState({ prop1: [0, 0] });
 
-    expect(2).toBe(2);
-})
+    //         return () => {
+    //             return h(
+    //                 'div',
+    //                 state1
+    //             );
+    //         };
+    //     },
+    // });
 
-it.todo('error: should not allow create state from another state value')
-
-it.todo('error: should not allow create state from another state value (nested)')
-
-it.todo('error: should not allow serialization of statelink')
+    expect(JSON.stringify(state1.prop1))
+        // tslint:disable-next-line: max-line-length
+        .toEqual('Error: HOOKSTATE-109 [path: /]. See https://hookstate.js.org/docs/exceptions#hookstate-109')
+});
