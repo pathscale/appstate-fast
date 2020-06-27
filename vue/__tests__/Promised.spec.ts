f90d2db4136bedc53ae19eb0d80c93ee22db11a2
felipemuner <felipe.muner@gmail.com>
2020-06-27 12:48:05 +0700

    set up Promised.spec.ts and todo tests

--- vue/__tests__/Promised.spec.ts
+++ vue/__tests__/Promised.spec.ts
@@ -2,10 +2,29 @@ import { useState, self } from "../src";
 import { computed } from "vue";
 import { h } from "vue";
 
-it.todo('')
+it.todo('primitive: should rerender used on promise resolve')
+it.todo('array: should rerender used on promise resolve')
+it.todo('array: should rerender used on promise resolve (global)')
+it.todo('array: should rerender used on promise resolve (global promise)')
+it.todo('primitive: should rerender used on promise resolve manual')
+it.todo('primitive: should rerender used on promise resolve second')
+it.todo('primitive: should rerender used on promise resolved')
+it.todo('primitive: should rerender used on promise reject')
+it.todo('primitive: should rerender used on promise rejected')
+it.todo('primitive: should rerender used on promise resolve init')
+it.todo('primitive: should rerender used on promise resolve init global')
+it.todo('primitive: should rerender used on promise reject init global')
 
-it.todo('')
 
-it.todo('')
 
-it.todo('')
+
+
+
+
+
+
+
+
+
+
+
