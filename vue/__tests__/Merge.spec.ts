bd1832cca9468e296df7116c09843414e8b35a50
felipemuner <felipe.muner@gmail.com>
2020-06-28 16:32:00 +0700

    Merge.spec.ts --- string: should rerender used after merge update

--- vue/__tests__/Merge.spec.ts
+++ vue/__tests__/Merge.spec.ts
@@ -3,32 +3,57 @@ import { useState, self, State } from "../src";
 import { h, nextTick } from "vue";
 
 test("primitive: should rerender used after merge update", async () => {
-    let renderTimes = 0;
+  let renderTimes = 0;
 
-    let result: State<number> = {} as any;
+  let result: State<number> = {} as any;
 
-    const wrapper = mount({
-        setup() {
-            result = useState(1);
-            return () => {
-                ++renderTimes;
-                return h(
-                    "div",
-                    Object.keys(result).map((x) => x)
-                );
-            };
-        },
-    });
-    expect(renderTimes).toStrictEqual(1);
-    expect(result[self].get()).toStrictEqual(1);
+  const wrapper = mount({
+      setup() {
+          result = useState(1);
+          return () => {
+              ++renderTimes;
+              return h(
+                  "div",
+                  Object.keys(result).map((x) => x)
+              );
+          };
+      },
+  });
+  expect(renderTimes).toStrictEqual(1);
+  expect(result[self].get()).toStrictEqual(1);
 
-    result[self].merge((p) => p + 1);
-    await nextTick();
-    expect(renderTimes).toStrictEqual(2);
-    expect(result[self].get()).toStrictEqual(2);
+  result[self].merge((p) => p + 1);
+  await nextTick();
+  expect(renderTimes).toStrictEqual(2);
+  expect(result[self].get()).toStrictEqual(2);
 });
 
-it.todo("string: should rerender used after merge update");
+test('string: should rerender used after merge update', async () => {
+  let renderTimes = 0;
+
+  let result: State<string> = {} as any;
+
+  const wrapper = mount({
+      setup() {
+          result = useState('str');
+          return () => {
+              ++renderTimes;
+              return h(
+                  "div",
+                  Object.keys(result).map((x) => x)
+              );
+          };
+      },
+  });
+  expect(renderTimes).toStrictEqual(1);
+  expect(result[self].get()).toStrictEqual('str');
+  
+  result[self].merge('str');
+  await nextTick();
+  expect(renderTimes).toStrictEqual(2);
+  expect(result[self].get()).toStrictEqual('strstr');
+})
+
 it.todo("object: should rerender used after merge update");
 it.todo("object: should rerender used after merge insert");
 it.todo("object: should rerender used after merge delete");
