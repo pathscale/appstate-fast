2a4241cb082a887e14ef2739b0ffb9a5df13ac8f
felipemuner <felipe.muner@gmail.com>
2020-06-28 10:21:06 +0700

    Batch.spec.ts --- object: should rerender used via nested batch double

--- vue/__tests__/Batch.spec.ts
+++ vue/__tests__/Batch.spec.ts
@@ -1,5 +1,5 @@
 import { mount } from "@vue/test-utils";
-import { useState, self, State } from "../src";
+import { useState, self, State, none} from "../src";
 import { h, nextTick } from "vue";
 
 test('object: should rerender used via nested batch update', async () => {
@@ -67,7 +67,7 @@ test('object: should rerender used via nested batch merge', async () => {
             field1: p.field1 +1,
             field2: p.field2 + 'str'
         }))
-    })
+    }, null)
     
     await nextTick();
 
@@ -79,10 +79,80 @@ test('object: should rerender used via nested batch merge', async () => {
     expect(Object.keys(result[self].get())).toEqual(['field1', 'field2']);
 })
 
-it.todo('object: should rerender used via nested batch double')
+test('object: should rerender used via nested batch double', async () => {
+    let renderTimes = 0;
+    let result: State<{field1:number, field2: string}> = {} as any;
+    const wrapper = mount({      
+        setup() {            
+            result = useState({
+              field1: 0,
+              field2: 'str',
+            });
+            return () => {
+                ++renderTimes;
+                return h(
+                    "div",
+                    result
+                );
+            };
+        },
+    });
+    expect(renderTimes).toStrictEqual(1);
+    expect(result.field1[self].get()).toStrictEqual(0);
+    expect(result.field2[self].get()).toStrictEqual('str');
+
+    result[self].map(()=>{
+        result.field2[self].set(p => p + '-before-')
+        result[self].map(()=>{
+            result[self].merge(p=>({
+                field1: p.field1 +1,
+                field2: p.field2 + 'str'
+            }))
+        }, null)
+        result.field2[self].set(p => p + '-after-')
+    }, null)
+    
+    await nextTick();
+
+    expect(renderTimes).toStrictEqual(2);
+    expect(result.field1[self].get()).toStrictEqual(1);
+    expect(result.field2[self].get()).toStrictEqual('str-before-str-after-');
+    expect(Object.keys(result)).toEqual(['field1', 'field2']);
+    expect(Object.keys(result[self].get())).toEqual(['field1', 'field2']);
+
+})
+
+// test('object: should rerender used via nested batch promised', async () => {
+    
+//     let renderTimes = 0;
+//     let result: State<number> = 0 as any;
+//     const wrapper = mount({
+//         setup() {
+//             result = useState(0);
+//             return () => {
+//                 ++renderTimes;
+//                 return h(
+//                     "div",
+//                     result.map((x) => x.value)
+//                 );
+//             };
+//         },
+//     });
+
+    //TO DO !
+
+    // expect(renderTimes).toStrictEqual(1);
+    // expect(result[self].get()).toStrictEqual(0);
+    // const promise = new Promise<number>(resolve => setTimeout(async () => {
+    //     resolve(100)
+    //     await nextTick();
+    // }, 500))
+    // result[self].set(promise);
+// })
 
 it.todo('object: should rerender used via nested batch promised')
 
 it.todo('object: should rerender used via nested batch promised manual')
 
-it.todo('object: should rerender used via scoped batched updates')
+test('object: should rerender used via scoped batched updates', async () => {
+})
