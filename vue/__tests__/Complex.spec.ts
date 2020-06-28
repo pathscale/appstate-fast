bfda105374d31437fe5f90952c8b86268dbd9618
felipemuner <felipe.muner@gmail.com>
2020-06-28 12:47:44 +0700

    Complex.spec.ts --- complex: should rerender unused when new element

--- vue/__tests__/Complex.spec.ts
+++ vue/__tests__/Complex.spec.ts
@@ -71,7 +71,7 @@ test('complex: should rerender used via nested', async () => {
 test('complex: should rerender used when set to the same', async () => {
   let renderTimes = 0;
 
-  let result: State<[{field:number}]> = {} as any;
+  let result: State<{field:number}[]> = {} as any;
 
   const wrapper = mount({
       setup() {
@@ -100,7 +100,49 @@ test('complex: should rerender used when set to the same', async () => {
   expect(Object.keys(result[0][self].get())).toEqual(['field']);
 })
 
-it.todo('complex: should rerender unused when new element')
+test('complex: should rerender unused when new element', async () => {
+
+let renderTimes = 0;
+
+  let result: State<{field1:number, field2: string}[]> = {} as any;
+
+  const wrapper = mount({      
+      setup() {            
+          result = useState([{
+            field1: 0,
+            field2: 'str'
+          }]);
+          return () => {
+              ++renderTimes;
+              return h(
+                  "div",
+                  Object.keys(result).map((x) => x)
+              );
+          };
+      },
+  });
+
+  expect(renderTimes).toStrictEqual(1);
+
+  result[0]['field3'][self].set(1);
+  await nextTick();
+  
+  expect(renderTimes).toStrictEqual(2);
+    expect(result[0][self].get()).toEqual({
+        field1: 0,
+        field2: 'str',
+        field3: 1
+    });
+    expect(Object.keys(result[0])).toEqual(['field1', 'field2', 'field3']);
+    expect(Object.keys(result[0][self].get())).toEqual(['field1', 'field2', 'field3']);
+    expect(result[0][self].get().field1).toStrictEqual(0);
+    expect(result[0][self].get().field2).toStrictEqual('str');
+    // tslint:disable-next-line: no-string-literal
+    expect(result[0][self].get()['field3']).toStrictEqual(1);
+
+})
+
+
 it.todo('complex: should not rerender unused property')
 it.todo('complex: should not rerender unused self')
 it.todo('complex: should delete property when set to none')
