9c8bb282b579ca3c4da092499707dcfa5409127a
felipemuner <felipe.muner@gmail.com>
2020-06-28 10:43:44 +0700

    Complex.spec.ts --- complex: should rerender used

--- vue/__tests__/Complex.spec.ts
+++ vue/__tests__/Complex.spec.ts
@@ -1,8 +1,40 @@
-import { useState, self } from "../src";
-import { computed } from "vue";
-import { h } from "vue";
+import { mount } from "@vue/test-utils";
+import { useState, self, State, none} from "../src";
+import { h, nextTick } from "vue";
 
-it.todo('complex: should rerender used')
+test('complex: should rerender used', async () => {
+  let renderTimes = 0;
+
+    let result: State<{field1:number, field2: string}[]> = {} as any;
+
+    const wrapper = mount({      
+        setup() {            
+            result = useState([{
+              field1: 0,
+              field2: 'str'
+            }]);
+            return () => {
+                ++renderTimes;
+                return h(
+                    "div",
+                    Object.keys(result).map((x) => x)
+                );
+            };
+        },
+    });
+
+    expect(renderTimes).toStrictEqual(1);
+    expect(result[0][self].get().field1).toStrictEqual(0);
+
+    result[0].field1[self].set(p => p + 1);
+    await nextTick();
+
+    expect(renderTimes).toStrictEqual(2);
+    expect(result[self].get()[0].field1).toStrictEqual(1);
+    expect(Object.keys(result[0])).toEqual(['field1', 'field2']);
+    expect(Object.keys(result[self].get()[0])).toEqual(['field1', 'field2']);
+})
+
 it.todo('complex: should rerender used via nested')
 it.todo('complex: should rerender used when set to the same')
 it.todo('complex: should rerender unused when new element')
