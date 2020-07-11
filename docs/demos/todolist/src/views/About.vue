6458d88241ffacf1391d0da59f1ddc34ad66d052
felipemuner <felipe.muner@gmail.com>
2020-07-11 21:08:53 +0700

    import ts/tsx files to update it and finish the todolist task

--- docs/demos/todolist/src/views/About.vue
+++ docs/demos/todolist/src/views/About.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <About msg="about msg" />
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import About from '@/components/About.vue'
-
 export default {
-  name: 'About',
-  components: {
-    About
-  }
-}
+    name: "About"
+};
 </script>
--- docs/demos/todolist/src/views/Home.vue
+++ docs/demos/todolist/src/views/Home.vue
@@ -1,25 +1,77 @@
 <template>
-  <div>
-    {{state}}
-    <!-- <button @click="increment">
-      Count is: {{ state.count }}, double is: {{ state.double }}
-    </button> -->
-  </div>
+    <div style="display: flex;">
+        <div style="flexGrow: 2;" />
+        <div
+            style="
+        minHeight: 100vh;
+        display: flex;
+        flexDirection: column;
+        justifyContent: center;
+        fontSize: 28;
+        color: 'white';
+      "
+        >
+            <div style="minWidth: 400; maxWidth: 800; padding: 20;">
+                <div style="marginBottom: 30;">
+                    This is
+                    <a
+                        style="color: #09d3ac;"
+                        href="https://github.com/avkonst/hookstate"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >Hookstate</a
+                    >
+                    demo application. Source code is on
+                    <a
+                        style="color: #09d3ac;"
+                        href="https://github.com/avkonst/hookstate/tree/master/docs/demos/todolist"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >GitHub</a
+                    >.
+                </div>
+                <SettingsViewer />
+                <TasksTotal />
+                <TasksViewer />
+            </div>
+        </div>
+        <div style={{ flexGrow: 2 }} />
+    </div>
 </template>
 
 <script>
-import { reactive, computed, ref, onMounted, onCreated } from 'vue'
+import { reactive, computed, ref, onMounted, onCreated } from "vue";
 import { createState, useState } from "@hookstate/vue";
+import { TasksViewer } from "../components/TasksViewer";
+import { SettingsViewer } from "../components/SettingsViewer";
+import { TasksTotal } from "../components/TasksTotal";
 
 export default {
-  setup() {
-    onMounted(() => {
-      console.log('component is mounted!')
-    })
-    const state = useState(createState(50));
-    return {
-      state
+    setup() {
+        onMounted(() => {
+            console.log("component is mounted!");
+        });
+        const state = useState(
+            createState([
+                { id: 1, text: "Discover Hookstat" },
+                { id: 2, text: "Replace Redux by Hookstate" },
+                { id: 3, text: "Enjoy simpler code and faster application" }
+            ])
+        );
+        return {
+            state
+        };
     }
-  }
-}
+};
 </script>
+
+<style scoped>
+.todo-length {
+    background: darkslategray;
+    color: white;
+    width: 20%;
+    font-size: 20px;
+    margin: auto;
+    margin-bottom: 20px;
+}
+</style>
