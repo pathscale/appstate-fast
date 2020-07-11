847c9584dd9414bd512c8cf538629aa8a2c1d8d2
felipemuner <felipe.muner@gmail.com>
2020-07-11 14:19:33 +0700

    set up vue3 successfuly - hello world

--- docs/demos/todolist/src/components/HelloWorld.vue
+++ docs/demos/todolist/src/components/HelloWorld.vue
@@ -33,14 +33,13 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
 
-export default Vue.extend({
+export default {
   name: 'HelloWorld',
   props: {
     msg: String,
   },
-});
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- docs/demos/todolist/src/router/index.ts
+++ docs/demos/todolist/src/router/index.ts
@@ -1,28 +1,24 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue'
 
-Vue.use(VueRouter)
-
-  const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes: Array<RouteRecordRaw> = [
+{
+  path: '/',
+  name: 'Home',
+  component: Home
+},
+{
+  path: '/about',
+  name: 'About',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+}
 ]
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
