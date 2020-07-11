9681e533ee4bce7ef407f7948027eea73fe8a685
felipemuner <felipe.muner@gmail.com>
2020-07-11 14:23:17 +0700

    test about page

--- docs/demos/todolist/src/views/About.vue
+++ docs/demos/todolist/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <About msg="about msg" />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import About from '@/components/About.vue'
+
+export default {
+  name: 'About',
+  components: {
+    About
+  }
+}
+</script>
