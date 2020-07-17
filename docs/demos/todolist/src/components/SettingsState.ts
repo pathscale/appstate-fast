59b408101a5da22c354dc3ba39cbe6ec5b818dc1
Anton Kudryavtsev <boblobl4@gmail.com>
2020-07-17 07:51:16 +0300

    todolist demo

--- docs/demos/todolist/src/components/SettingsState.ts
+++ docs/demos/todolist/src/components/SettingsState.ts
@@ -1,11 +1,11 @@
-import { createState, useState, self } from '@hookstate/core';
+import { createState, useState, self } from "@hookstate/vue";
 
 export const useSettingsState = createState({
     isEditableInline: true,
     isScopedUpdateEnabled: true,
-    isHighlightUpdatesEnabled: true
+    isHighlightUpdateEnabled: true
 })[self].map(s => () => {
-    const state = useState(s)
+    const state = useState(s);
 
     // This function wraps the state by an interface,
     // i.e. the state link is not accessible directly outside of this module.
@@ -13,25 +13,28 @@ export const useSettingsState = createState({
     // Both options are valid and you need to use one or another,
     // depending on your circumstances. Apply your engineering judgement
     // to choose the best option. If unsure, exposing the state directly
-    // like it is done in the TasksState.ts is a safe bet.        
-    return ({
+    // like it is done in the TasksState.ts is a safe bet.
+    return {
         get isEditableInline() {
-            return state.isEditableInline.get()
+            return state.isEditableInline.get();
         },
-        toogleEditableInline() {
-            state.isEditableInline.set(p => !p)
+        toggleEditableInline() {
+            state.isEditableInline.set(p => !p);
+            console.log(state.isEditableInline.get());
         },
         get isScopedUpdateEnabled() {
-            return state.isScopedUpdateEnabled.get()
+            return state.isScopedUpdateEnabled.get();
         },
-        toogleScopedUpdate() {
-            state.isScopedUpdateEnabled.set(p => !p)
+        toggleScopedUpdate() {
+            state.isScopedUpdateEnabled.set(p => !p);
+            console.log(state.isScopedUpdateEnabled.get());
         },
         get isHighlightUpdateEnabled() {
-            return state.isHighlightUpdatesEnabled.get()
+            return state.isHighlightUpdateEnabled.get();
         },
-        toogleHighlightUpdate() {
-            state.isHighlightUpdatesEnabled.set(p => !p)
+        toggleHighlightUpdate() {
+            state.isHighlightUpdateEnabled.set(p => !p);
+            console.log(state.isHighlightUpdateEnabled.get());
         }
-    })   
-})
+    };
+});
---
+++ docs/demos/todolist/src/components/SettingsViewer.vue
@@ -0,0 +1,54 @@
+<template>
+  <div
+    style="border: solid;border-width: 1px;border-color: #09d3ac;margin-bottom: 30px;font-size: 0.8em;display: flex;"
+  >
+    <div style="flex-grow: 2; display: flex;">
+      <div>
+        <input
+          style="transform: scale(1.6); margin-left: 20px;"
+          type="checkbox"
+          :checked="settingsState.isEditableInline"
+          @change="settingsState.toggleEditableInline"
+        />
+      </div>
+      <div style="padding-left: 10px; padding-bottom: 10px;">edit inline</div>
+    </div>
+    <div style="flex-grow: 2; display: flex;">
+      <div>
+        <input
+          style="transform: scale(1.6); margin-left: 20px;"
+          type="checkbox"
+          :checked="settingsState.isScopedUpdateEnabled"
+          @change="settingsState.toggleScopedUpdate"
+        />
+      </div>
+      <div style="padding-left: 10px; padding-bottom: 10px;">use scoped state</div>
+    </div>
+    <div style="flex-grow: 2; display: flex;">
+      <div>
+        <input
+          style="transform: scale(1.6); margin-left: 20px;"
+          type="checkbox"
+          :checked="settingsState.isHighlightUpdateEnabled"
+          @change="settingsState.toggleHighlightUpdate"
+        />
+      </div>
+      <div style="padding-left: 10px;padding-bottom: 10px;padding-right: 10px;">highlight updates</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { useSettingsState } from "./SettingsState";
+
+export default defineComponent({
+  name: "SettingsViewer",
+  setup() {
+    const settingsState = useSettingsState();
+    return { settingsState };
+  }
+});
+</script>
+
+<style></style>
