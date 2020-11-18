ddcdf1dcbbc500a1a5585758f089e1901ccd80cc
David <david.cedresx@gmail.com>
2020-11-18 19:03:47 -0400

    add basic watcheffect test

--- vue/__tests__/simple.spec.ts
+++ vue/__tests__/simple.spec.ts
@@ -1,29 +1,56 @@
-import { h, watchEffect, onMounted } from "vue"
+import { h, watchEffect, reactive } from "vue"
 import { useState } from "../src";
 import { mount } from "@vue/test-utils";
 
-describe("Test nested state", () => {    
-    it("WatchEffect should run indepedently for nested states", () => {
+describe("watchEffect works correctly without browser [oscar claims it does not]", () => {
+    it("watchEffect runs once per update", done => {
         mount({
             name: 'Test',
             setup() {
-                const state = useState({ a: 1, b: 2 });
-
-                /** WatchEffect runs one time and then tracks its dependencies
-                 *  So the fact that b changes 
-                 */
+                const state = reactive({ a: 0 });
 
                 watchEffect(() => {
-                    console.log('side effect for state.a', state.a.value)
+                    console.log('side effect for a [should run 3 times]', state.a)
                 });
+
+                setInterval(() => {
+                    state.a += 1;
+                    state.a === 3 && done()
+                }, 200)
+            },
+            render() {
+                return h("h1", {}, "Hello world");
+            }
+        })
+    });
+});
+
+
+describe("Test nested state", () => {    
+    it("mutation of a substate should not trigger updates for other substates", done => {
+
+        let acc = 0;
+
+        mount({
+            name: 'Test',
+            setup() {
+                const state = useState({ a: 0, b: 2 });
 
                 watchEffect(() => {
-                    console.log('side effect for state.b', state.b.value)
+                    // should run only once
+                    console.log('side effect for state.b [should run only once]', state.b.value)
                 });
 
-                onMounted(() => {
-                    state.a.set(p => p +1)
-                })
+                // watchEffect(() => {
+                //     console.log('side effect for state.a [should run many times]', state.a.value)
+                // })
+
+                setInterval(() => {
+                    state.a.set(p => p+1)
+                    acc += 1
+                    acc === 3 && done()
+                }, 100)
+
             },
             render() {
                 return h("h1", {}, "Hello world");
